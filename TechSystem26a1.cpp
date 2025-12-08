1794da614bd474963137414d7747545eba46ef11
el12391 <eliavkatz181@gmail.com>
2025-12-08 17:18:21 +0200

    does add Course and removeCours

--- TechSystem26a1.cpp
+++ TechSystem26a1.cpp
@@ -53,13 +53,33 @@ StatusType TechSystem::addCourse(int courseId, int points)
 {
     if (courseId <= 0 || points <= 0) {return StatusType::INVALID_INPUT;}
     try {
-        Course course = new Course
+        std::shared_ptr<Course> coursePtr =
+                std::make_shared<Course>(courseId, points);
+        this->courseSystem.insert(coursePtr);
+        return StatusType::SUCCESS;
+    } catch (std::bad_alloc) {
+        return StatusType::ALLOCATION_ERROR;
+    } catch (...) {
+        return StatusType::FAILURE;
     }
     return StatusType::FAILURE;
 }
 
 StatusType TechSystem::removeCourse(int courseId)
 {
+    if (courseId <= 0) {return StatusType::INVALID_INPUT;}
+    try {
+        std::shared_ptr<Course> coursePtr = std::make_shared<Course>(courseId);
+        if (this->courseSystem.find(coursePtr)->height != 0){
+            return StatusType::FAILURE;
+        }
+        this->courseSystem.remove(coursePtr);
+        return StatusType::SUCCESS;
+    } catch (std::bad_alloc) {
+        return StatusType::ALLOCATION_ERROR;
+    } catch (...) {
+        return StatusType::FAILURE;
+    }
     return StatusType::FAILURE;
 }
 
